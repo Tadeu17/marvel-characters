9711ba7c91c39aa845a77a8043d96cac7aede34f
Tadeu17 <17.tadeu.1989@gmail.com>
2020-06-22 20:24:47 +0100

    axios fix

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import axios from './plugins/axios'
+
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
@@ -12,5 +14,6 @@ new Vue({
   router,
   store,
   vuetify,
+  axios,
   render: h => h(App)
 }).$mount('#app')
