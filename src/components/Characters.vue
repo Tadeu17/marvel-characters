b6433aa819a4fab179585215c24fd15315ccf84d
Tadeu17 <17.tadeu.1989@gmail.com>
2020-06-22 19:50:04 +0100

    added characters component and lintfixed whole project

---
+++ src/components/Characters.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h3>This is a characters.vue component {{ name }}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Characters'
+}
+</script>
+
+<style lang="scss" scoped>
+.characters-list {
+  background-color: #333;
+}
+</style>
