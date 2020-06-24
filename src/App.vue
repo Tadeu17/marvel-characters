2697c748c0ac467c172dfce26141a18c62858c18
Tadeu17 <17.tadeu.1989@gmail.com>
2020-06-24 20:26:39 +0100

    changed navbar display

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,46 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <router-link to="/">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            :src="require('./assets/marvel-logo.png')"
+            transition="scale-transition"
+            width="120"
+        /></router-link>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-menu :value="openMenu">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="primary"
+            elevation="0"
+            v-bind="attrs"
+            v-on="on"
+            width="120"
+            height="60"
+          >
+            Menu
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item to="/">
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/about">
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <!-- <v-btn to="/about" text>
+        <span class="mr-2">About</span>
+        <v-icon>mdi-help</v-icon>
+      </v-btn> -->
     </v-app-bar>
 
     <v-main>
@@ -41,6 +52,11 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      openMenu: true
+    }
+  }
 }
 </script>
