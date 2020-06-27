9517932578a37f9800de24455d8f2f225b54db65
Tadeu17 <17.tadeu.1989@gmail.com>
2020-06-27 20:59:29 +0100

    fixed warning in list card spec

--- tests/unit/components/list-card.spec.js
+++ tests/unit/components/list-card.spec.js
@@ -1,9 +1,15 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
 import { shallowMount } from '@vue/test-utils'
 import ListCard from '@/components/characters/ListCard'
+Vue.use(Vuetify)
 
 describe('base/ListCard', () => {
+  let vuetify = new Vuetify()
+
   it('should render the character title and an image', () => {
     const wrapper = shallowMount(ListCard, {
+      vuetify,
       propsData: {
         character: { id: 1, name: '3-D Man', imageUrl: 'an image url' }
       }
