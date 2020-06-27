5cb8bfb67cb27318ddb64ac1814ef52ec789114e
Tadeu17 <17.tadeu.1989@gmail.com>
2020-06-27 19:26:09 +0100

    not using store directly in mutations tests

--- tests/unit/store/mutations.spec.js
+++ tests/unit/store/mutations.spec.js
@@ -1,21 +1,24 @@
-import store from '@/store'
 import mockData from '../mockData/index'
 import { resetState } from '../helpers'
 import { SET_CHARACTERS } from '@/store/mutations.type'
+import mutations from '@/store/mutations'
+import state from '@/store/state'
 
 describe('Mutations', () => {
-  beforeEach(resetState)
+  beforeEach(() => {
+    resetState()
+  })
 
   it('Should correctly set the characters', () => {
-    store.commit(SET_CHARACTERS, {
+    mutations[SET_CHARACTERS](state, {
       characters: mockData.CHARACTERS
     })
 
-    expect(store.state.characters).toMatchObject(mockData.CHARACTERS)
-    expect(store.state.characters[0].imageUrl).toBe(
+    expect(state.characters).toMatchObject(mockData.CHARACTERS)
+    expect(state.characters[0].imageUrl).toBe(
       mockData.CHARACTERS[0].imageUrl
     )
-    expect(store.state.characters[1].imageUrl).toBe(
+    expect(state.characters[1].imageUrl).toBe(
       mockData.CHARACTERS[1].imageUrl
     )
   })
