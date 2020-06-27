0c001063cc727bc25fd81d528a45565e00226a9b
Tadeu17 <17.tadeu.1989@gmail.com>
2020-06-27 18:31:13 +0100

    used get instead of find in list card spec

--- tests/unit/components/list-card.spec.js
+++ tests/unit/components/list-card.spec.js
@@ -8,10 +8,10 @@ describe('base/ListCard', () => {
         character: { id: 1, name: '3-D Man', imageUrl: 'an image url' }
       }
     })
-    const cardTitle = wrapper.find('.character-card-container h2')
+    const cardTitle = wrapper.get('.character-card-container h2')
     expect(cardTitle.text()).toBe('3-D Man')
 
-    const cardImage = wrapper.find('.character-card-container img')
+    const cardImage = wrapper.get('.character-card-container img')
     console.log(cardTitle, cardImage)
     expect(cardImage.attributes('src')).toBe('an image url')
   })
